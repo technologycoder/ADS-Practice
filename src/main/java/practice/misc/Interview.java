1b36f3a017330c823b74d9d241fcaa5c67b0922e
TechCoder <technology.coder@gmail.com>
2016-11-27 19:26:52 -0600

    adding code

---
+++ src/main/java/practice/misc/Interview.java
@@ -0,0 +1,56 @@
+package practice.misc;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class Interview {
+
+	public static void main(String[] args) throws Exception {
+
+		permute("ABC", "");
+
+	}
+
+	public static void permute(String str, String prefix) {
+
+		if (str.length() == 0) {
+			System.out.println(prefix);
+		} else {
+			for (int i = 0; i < str.length(); ++i) {
+
+				permute(str.substring(0, i) + str.substring(i + 1), prefix + str.charAt(i));
+			}
+		}
+
+	}
+
+	public static void main1(String[] args) throws Exception {
+
+		Stream<String> stream = Files.lines(Paths.get("sample.txt"));
+
+		final Map<String, Long> letterToCount = stream.map(w -> w.split("")).flatMap(Arrays::stream)
+				.collect(groupingBy(identity(), counting()));
+
+		System.out.println(letterToCount);
+
+		String txt = "abcdefghijklmnopqrstuvwxyz_";
+
+		List<String> str = Arrays.asList(txt.split(""));
+
+		System.out.println(str);
+
+		str.sort((String a, String b) -> (int) (letterToCount.get(a) - letterToCount.get(b)));
+
+		System.out.println(str);
+
+	}
+
+}
